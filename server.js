92a67dfd442cc425b912a852b94bd1f3ca397a17
srinath <srinmolugu@gmail.com>
2024-04-02 10:14:58 +0530

    Added query Params

--- server.js
+++ server.js
@@ -1,78 +1,87 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require('./models/productModel')
+const Product = require("./models/productModel");
 const app = express();
 
-
-app.use(express.json())
+app.use(express.json());
 //routes
 app.get("/", (req, res) => {
   res.send("Hi sd");
 });
 
-//getting whole products
-app.get('/products', async(req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
+//getting whole products - added filtering as well
+app.get("/products", async (req, res) => {
+  const filters = req.query || "";
+  let filteredResults = [];
+  try {
+    const products = await Product.find({});
+    filters.price ? 
+    products.filter((ele) => {
+      if (ele.price == filters.price) {
+        filteredResults.push(ele);
+      }
+    }) : filteredResults = [...products]
+
+    res.status(200).json(filteredResults);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 //getting product by id
-app.get('/products/:id', async(req, res) =>{
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // update a product
-app.put('/products/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-        // we cannot find any product in database
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
-        }
-        const updatedProduct = await Product.findById(id);
-        res.status(200).json(updatedProduct);
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    // we cannot find any product in database
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
     }
-})
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // delete a product
-app.delete('/products/:id', async(req, res) =>{
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndDelete(id);
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
-        }
-        res.status(200).json(product);
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
     }
-})
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.post('/products', async(req, res) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(200).json(product);
-        
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message})
-    }
-})
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
 
 mongoose
   .connect(
@@ -81,8 +90,8 @@ mongoose
   .then(() => {
     console.log("connected to mongodb");
     app.listen(3000, () => {
-        console.log("node app running good");
-      });
+      console.log("node app running good");
+    });
   })
   .catch((error) => {
     console.log("error", error);
